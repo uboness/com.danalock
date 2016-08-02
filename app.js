a6caaf43c23a45761581ec20dac0cfbbb9185df0
Emile Nijssen <emile@athom.nl>
2016-08-02 14:57:57 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function init() {
+
+	Homey.log("com.danalock running...");
+
+}
+
+module.exports.init = init;
---
+++ drivers/danalock_v2/driver.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const path			= require('path');
+const ZwaveDriver	= require('homey-zwavedriver');
+
+module.exports = new ZwaveDriver( path.basename(__dirname), {
+	debug: true,
+	capabilities: {
+		'locked': {
+			'command_class'				: 'COMMAND_CLASS_DOOR_LOCK',
+			'command_get'				: 'DOOR_LOCK_OPERATION_GET',
+			'command_set'				: 'DOOR_LOCK_OPERATION_SET',
+			'command_set_parser'		: function( value ){
+				return {
+					'Door Lock Mode': ( value ) ? 'Door Secured' : 'Door Unsecured'
+				}
+			},
+			'command_report'			: 'DOOR_LOCK_OPERATION_REPORT',
+			'command_report_parser'		: function( report ){
+				return report['Door Lock Mode'] === 'Door Secured';
+
+			}
+		}
+	},
+	settings: {}
+})
