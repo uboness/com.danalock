0d4dd3e4f59ffb1293a735a202385dc6977428a0
RobinBol <robinbolscher@gmail.com>
2017-04-18 17:51:05 +0200

    Version bump (1.0.3): add productTypeId 3, fixes #1

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-"use strict";
+'use strict';
 
 const Log = require('homey-log').Log;
 
-function init() {
-
-	Homey.log("com.danalock running...");
-
-}
-
-module.exports.init = init;
+module.exports.init = () => {
+	console.log(`${Homey.manifest.id} running...`);
+};
