0d2e0b7927382baa4e7b11b9e0e9e2bf6735dc42
Robin Bolscher <robinbolscher@gmail.com>
2017-10-25 20:23:52 +0200

    Update meshdriver and device.js accordingly

--- drivers/danalock_v2/device.js
+++ drivers/danalock_v2/device.js
@@ -3,7 +3,12 @@
 const ZwaveLockDevice = require('homey-meshdriver').ZwaveLockDevice;
 
 class DanalockV2 extends ZwaveLockDevice {
-
+	async onMeshInit() {
+		await super.onMeshInit();
+		this.registerCapability('locked', 'DOOR_LOCK');
+		this.registerCapability('locked', 'NOTIFICATION');
+		this.registerCapability('measure_battery', 'BATTERY');
+	}
 }
 
 module.exports = DanalockV2;
--- drivers/danalock_v3/device.js
+++ drivers/danalock_v3/device.js
@@ -3,7 +3,12 @@
 const ZwaveLockDevice = require('homey-meshdriver').ZwaveLockDevice;
 
 class DanalockV3 extends ZwaveLockDevice {
-
+	async onMeshInit() {
+		await super.onMeshInit();
+		this.registerCapability('locked', 'DOOR_LOCK');
+		this.registerCapability('locked', 'NOTIFICATION');
+		this.registerCapability('measure_battery', 'BATTERY');
+	}
 }
 
 module.exports = DanalockV3;
